8f1ed9a7e290b0d119a1970b9368bdcf22f707b1
dannycrews <dancrews@me.com>
2016-06-18 18:23:40 -0400

    Elaborate on problem statement

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // Build a simple server with Node.js that will serve up a web page that will
 // allow a user to query any Treehouse user's profile and display some of their
 // information
-// 
+
 // 1. Create a web server
+
 // 2. Handle HTTP route GET / and POST / i.e. Home
+//    if url == / and GET
+//      show search
+//    if url == / and POST
+//      redirect to /:username
+
 // 3. Handle HTTP route GET /:username
+//    if url == /:username
+//      get json from treehouse
+//        on 'end'
+//          show profile
+//        on 'error'
+//          show error
+
 // 4. Functions that handle reading of files and parsing results
-// 
+
