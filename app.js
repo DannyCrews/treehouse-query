bb77690d1859e910c550b45e00b3c51876bd4ff9
dannycrews <dancrews@me.com>
2016-06-18 19:18:14 -0400

    Set up home GET route

--- app.js
+++ app.js
@@ -3,12 +3,29 @@
 // information
 
 // 1. Create a web server
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+   homeRoute(req, res);
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 // 2. Handle HTTP route GET / and POST / i.e. Home
+homeRoute = (req, res) => {
 //    if url == / and GET
 //      show search
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Header\n');
+  res.write('Search\n');
+  res.end('Footer\n');
 //    if url == / and POST
 //      redirect to /:username
+};
 
 // 3. Handle HTTP route GET /:username
 //    if url == /:username
@@ -19,18 +36,3 @@
 //          show error
 
 // 4. Functions that handle reading of files and parsing results
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
