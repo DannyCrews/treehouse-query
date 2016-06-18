bd9f4d51d8813657552e1df1d35820e16aee392d
dannycrews <dancrews@me.com>
2016-06-18 18:27:59 -0400

    Just cut and paste the example Node server code

--- app.js
+++ app.js
@@ -20,3 +20,17 @@
 
 // 4. Functions that handle reading of files and parsing results
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
