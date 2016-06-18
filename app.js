4aa4970dd80622393d04d4018c2c23329267d24c
dannycrews <dancrews@me.com>
2016-06-18 18:06:47 -0400

    initial commit of problem statement

---
+++ app.js
@@ -0,0 +1,9 @@
+// Build a simple server with Node.js that will serve up a web page that will
+// allow a user to query any Treehouse user's profile and display some of their
+// information
+// 
+// 1. Create a web server
+// 2. Handle HTTP route GET / and POST / i.e. Home
+// 3. Handle HTTP route GET /:username
+// 4. Functions that handle reading of files and parsing results
+// 
